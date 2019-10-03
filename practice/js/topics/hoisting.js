5fe23945a130ca36ededf92a2aba2187764b8ef8
yenkpt <yenkaphu96pnv@gmail.com>
2019-10-03 18:55:54 +0700

    feat(JAVASCRIPT|Front): learn Javascript hoisting

---
+++ practice/js/topics/hoisting.js
@@ -0,0 +1,11 @@
+// Example 1:
+  a1 = 5;
+  var elem1 = document.getElementById("demo1");
+  elem1.innerHTML = a1;
+  var a1;
+
+// Example 2:
+  var a2;
+  a2 = 5;
+  var elem2 = document.getElementById("demo2");
+  elem2.innerHTML = a2;
